bec4e734fc2cce3bf152acbfb66cfe58a01de842
Brie <Bss1089@gmail.com>
2025-05-23 17:50:18 -0400

    fixed home controller

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,6 @@
-const Creator = require("../models/Creator");
-
-
 module.exports = {
   getIndex: (req, res) => {
-                const creator = Creator.findById(req.params.id).lean();
-                const User = require("../models/User")
-
-    res.render("index.ejs", {user: req.user, creator});
+    res.render("index.ejs", { user: req.user || null });
   },
 };
 
